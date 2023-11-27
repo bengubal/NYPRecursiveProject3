d963730936a903d636685cc0346c8248790a89a0
bengubal <irembengubal@gmail.com>
2023-11-27 19:22:49 +0300

    Add project files.

---
+++ NYPRecursiveProject3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    //recursive function that separating the digits of a number
+    //and finding the sum of their values
+    static int SumOfDigits(int number)
+    {
+        if (number < 10)
+        {
+            return number;
+        }
+        else
+        {
+            return number % 10 + SumOfDigits(number / 10);
+        }
+    }
+    //main part of code
+    static void Main()
+    {
+        Console.Write("Enter a number (between 10 and 99,999): ");
+        int number = int.Parse(Console.ReadLine());
+        int result = SumOfDigits(number);
+        Console.WriteLine($"Sum of digits for {number}: {result}");
+    }
+}
